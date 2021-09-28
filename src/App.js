c3d018598f2a3217b69f231b09215a4c9e47fb06
LAPTOP-R8TAHJ1O\mac95 <mac95860@hotmail.com>
2021-09-27 22:04:34 -0500

    header setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
+import { MenuItem, FormControl, Select} from '@material-ui/core';
 
 function App() {
   return (
     <div className="App">
-      Hello
+      <div className = "app__header">
+        <h1>COVID 19 TRACKER</h1>
+        <FormControl className = "app__dropdown">
+          <Select variant = "outlined" value ="abc">
+
+          </Select>
+        </FormControl>
+      </div>
+      
 
     {/* Header */}
     {/* Title + Select input dropdown field */}
